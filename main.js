f11d538e0588459d0e98be093a18e267154d6efe
Lynne16 <76992610+Lynne16@users.noreply.github.com>
2021-10-13 19:50:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+noseX="";
+noseY="";
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	jumpsound=loadSound("jump.wav");
+	coinsound=loadSound("coin.wav");
+	kicksound=loadSound("kick.wav");
+	gameoversound=loadSound("gameover.wav");
+	mariodiesound=loadSound("mariodie.wav");
+
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('gameconsole');
+	instializeInSetup(mario);
+
+	posenet=ml5.poseNet(video,modelLoaded);
+	posenet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+	console.log('model Loaded!!!!!');
+}
+
+function gotPoses(results){
+
+	if(results.length > 0){
+		console.log(results);
+		noseX=results[0].pose.nose.x;
+		noseY=results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
